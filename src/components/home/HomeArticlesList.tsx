4dd6175f0cc69e12d3182f3f020ca8806006598a
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-01-08 21:51:13 +0700

    Adding data test id

--- src/components/home/HomeArticlesList.tsx
+++ src/components/home/HomeArticlesList.tsx
@@ -9,7 +9,7 @@ export default function HomeArticlesList({ posts }: Props) {
   return (
     <section className="my-5">
       <h2 className="my-4 text-xl font-bold">Latest Posts</h2>
-      <ul className="list-outside mb-8">
+      <ul className="list-outside mb-8" data-testid="latest-posts">
         {posts.map((post, index) => <HomeArticleItem key={index} post={post} />)}
       </ul>
     </section>
